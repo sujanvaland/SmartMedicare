ef498a61746f219c150eef5c0042db278ad656c4
Sujan <sujanvaland@gmail.com>
2022-09-11 09:18:43 +0530

    Add project files.

---
+++ Libraries/SmartStore.Data/Migrations/MigrationsConfiguration.cs
@@ -0,0 +1,54 @@
+﻿namespace SmartStore.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Setup;
+    using SmartStore.Core.Data;
+    using SmartStore.Utilities;
+
+    public sealed class MigrationsConfiguration : DbMigrationsConfiguration<SmartObjectContext>
+    {
+        public MigrationsConfiguration()
+        {
+            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "SmartStore.Core";
+
+            if (DataSettings.Current.IsSqlServer)
+            {
+                var commandTimeout = CommonHelper.GetAppSetting<int?>("sm:EfMigrationsCommandTimeout");
+                if (commandTimeout.HasValue)
+                {
+                    CommandTimeout = commandTimeout.Value;
+                }
+
+                CommandTimeout = 9999999;
+            }
+        }
+
+        public void SeedDatabase(SmartObjectContext context)
+        {
+            using (var scope = new DbContextScope(context, hooksEnabled: false))
+            {
+                Seed(context);
+                scope.Commit();
+            }
+        }
+
+        protected override void Seed(SmartObjectContext context)
+        {
+            context.MigrateLocaleResources(MigrateLocaleResources);
+            MigrateSettings(context);
+        }
+
+        public void MigrateSettings(SmartObjectContext context)
+        {
+
+        }
+
+        public void MigrateLocaleResources(LocaleResourcesBuilder builder)
+        {
+        }
+    }
+}
